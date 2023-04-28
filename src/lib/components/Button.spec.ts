f04171f2a9e73767c2dc31e23d129aed2b9aadfe
Viktor Zhechev <viktor.zhechev@amadeus.com>
2023-04-28 11:53:04 +0200

    Add config for tests and base spec files.

---
+++ src/lib/components/Button.spec.ts
@@ -0,0 +1,7 @@
+import Button from "./Button.svelte";
+
+describe("Example File", () => {
+    test("Sample test", () => {
+        expect(1 + 4).equal(5);
+    });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [
+		svelte({ hot: !process.env.VITEST }),
+	],
+	test: {
+		environment: 'jsdom',
+		globals: true
+	}
+})
